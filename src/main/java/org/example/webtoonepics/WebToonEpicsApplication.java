6fd67c309a8804cec01821e943d8b5ffd7773129
gwang1234 <qoi11@naver.com>
2024-09-05 13:13:32 +0900

    1

---
+++ src/main/java/org/example/webtoonepics/WebToonEpicsApplication.java
@@ -0,0 +1,13 @@
+package org.example.webtoonepics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebToonEpicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebToonEpicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webtoonepics/WebToonEpicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webtoonepics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebToonEpicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
