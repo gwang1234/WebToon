53cdaac029bde427d7e16e152c8705de68d0f91b
gwang1234 <qoi11@naver.com>
2024-09-05 14:37:10 +0900

    2

---
+++ src/main/java/org/example/webtoonepics/controller/LoginController.java
@@ -0,0 +1,11 @@
+package org.example.webtoonepics.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+    public String login() {
+        return "login";
+    }
+}
