966afd6aeef1abb80defa28e0d89071ef45965a2
one-west <wjdgkstj112@naver.com>
2024-09-07 02:38:25 +0900

    add 24/09/07
    LoginController 추가

--- src/main/java/org/example/webtoonepics/controller/LoginController.java
+++ src/main/java/org/example/webtoonepics/controller/LoginController.java
@@ -2,9 +2,13 @@ package org.example.webtoonepics.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    
+    @GetMapping("/login")
+    // This method will return the login page
     public String login() {
         return "login";
     }
