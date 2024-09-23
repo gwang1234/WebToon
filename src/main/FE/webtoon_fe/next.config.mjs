8ba481924ee70b95d7883e140c543e47dd07d161
jejunyoung <jejunyoung00@gmail.com>
2024-09-23 22:40:08 +0900

    feat: 메인 화면 구현
    메인 화면 구현

--- src/main/FE/webtoon_fe/next.config.mjs
+++ src/main/FE/webtoon_fe/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["via.placeholder.com"], // 외부 이미지 도메인 허용
+  },
 };
 
 export default nextConfig;
--- src/main/FE/webtoon_fe/src/app/page.tsx
+++ src/main/FE/webtoon_fe/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./CommonLayout/Header/page";
+import Main from "./Main/page";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
